94a4502c0eecdbd9b232b158667b833858605513
Benjamin Saunders <besaunde@sfu.ca>
2011-08-31 05:53:17 -0700

    Random-art urls and an index.

--- src/pages/art.lisp
+++ src/pages/art.lisp
@@ -6,6 +6,27 @@
 (defun make-seed ()
   (random (expt 2 48)))
 
+
+(defroute random-art "/art-index"
+  (with-output-to-string (s)
+    (fill-and-print-template (find-template "art-index")
+                             (list :small "128x128"
+                                   :large "512x512"
+                                   :seeds
+                                   (loop repeat 10 collect (list :seed (write-to-string (make-seed) :base 36)))
+)
+                             :stream s)))
+
+(defroute random-art "/random-art"
+  (redirect "/art"))
+
+(defroute random-art-sized "/random-art/:(x)x:(y)"
+  (redirect
+   (with-output-to-string (s)
+     (princ "/art/" s)
+     (write (make-seed) :base 36 :stream s)
+     (format s "/~Dx~D" (parse-integer x) (parse-integer y)))))
+
 (defroute art-seed-redirect "/art"
   (redirect (with-output-to-string (s)
               (princ "/art/" s)
