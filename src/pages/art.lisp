ace220f99673b5fe0ea78f4f0305501c44b08217
Benjamin Saunders <besaunde@sfu.ca>
2011-09-06 04:30:20 -0700

    Fixed the art pages' hunchentoot references.

--- src/pages/art.lisp
+++ src/pages/art.lisp
@@ -8,12 +8,13 @@
 
 
 (defroute art-seed-redirect "/random-art"
-  (redirect (with-output-to-string (s)
-              (princ "/art/" s)
-              (write (make-seed) :base 36 :stream s))))
+  (hunchentoot:redirect
+   (with-output-to-string (s)
+     (princ "/art/" s)
+     (write (make-seed) :base 36 :stream s))))
 
 (defroute random-art-sized "/random-art/:(x)x:(y)"
-  (redirect
+  (hunchentoot:redirect
    (with-output-to-string (s)
      (princ "/art/" s)
      (write (make-seed) :base 36 :stream s)
@@ -31,10 +32,10 @@
 
 
 (defroute art-size-redirect "/art/:seed"
-  (redirect (format nil "/art/~A/size/~Dx~D" seed 512 512)))
+  (hunchentoot:redirect (format nil "/art/~A/size/~Dx~D" seed 512 512)))
 
 (defroute art "/art/:seed/size/:(x)x:(y)"
-  (setf (content-type*) "image/png")
+  (setf (hunchentoot:content-type*) "image/png")
   (let ((seed (parse-integer seed :radix 36)))
     (setf seed
           (apply #'logxor (loop for i from 0
@@ -42,5 +43,5 @@
                                 until (= chunk 0)
                                 collect chunk)))
     (random-art:render-to-stream (random-art:generate :seed seed)
-                                 (send-headers)
+                                 (hunchentoot:send-headers)
                                  (parse-integer x) (parse-integer y))))
