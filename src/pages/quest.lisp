75eea46825d6d8102c6b6275c61591f58194d936
Benjamin Saunders <besaunde@sfu.ca>
2011-09-06 23:04:07 -0700

    XML header in feeds.

--- src/pages/quest.lisp
+++ src/pages/quest.lisp
@@ -25,7 +25,7 @@
          (base-url (format nil "http://~A/quest/~A" (host) id))
          (link (format nil "~A/feed" base-url)))
     (setf (content-type*) "application/rss+xml")
-    (with-html-output-to-string (s)
+    (with-html-output-to-string (s nil :prologue "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>")
       (:rss :version "2.0" :|xmlns:atom| "http://www.w3.org/2005/Atom"
             (:channel (:title (esc (title quest)))
                       (:link (esc link))
