7672a2f612db2c682c51f3d15cb6196d580e790f
Benjamin Saunders <besaunde@sfu.ca>
2011-09-13 21:56:25 -0700

    Find template dir by the simpler method of asdf:system-relative-pathname.

--- src/templates.lisp
+++ src/templates.lisp
@@ -1,6 +1,6 @@
 (in-package :quest)
 
-(defvar *template-dir* (truename (merge-pathnames "../templates/" (make-pathname :name nil :type nil :defaults (or #.*compile-file-pathname* *load-pathname*)))))
+(defvar *template-dir* (asdf:system-relative-pathname :quest "templates/"))
 
 (defun find-template (name)
   (aprog1 (make-pathname :name name :type "tmpl" :defaults *template-dir*)
