fc30c63c3b790bbda4b529aa3df801a857efa18a
Benjamin Saunders <besaunde@sfu.ca>
2011-09-13 21:56:05 -0700

    Added content-type column to media table.

--- src/media.lisp
+++ src/media.lisp
@@ -3,6 +3,7 @@
 ;;; UI note: Any media may be thumbnail; UI should default to setting the first upload.
 (defdao media ()
     ((content-id :col-type integer :initarg :content-id :reader content-id)
+     (content-type :col-type text :initarg :content-type :reader content-type)
      (name :col-type text :initarg :name :accessor name)
      (checksum :col-type bytea :initarg :checksum :accessor checksum))
   (:foreign-key content content-id id)
