4fbee20b75c4684d0d00889721692cfa37c0d653
Benjamin Saunders <besaunde@sfu.ca>
2011-09-19 20:00:20 -0700

    Login and logout return the user where they came from on success.

--- src/pages/login.lisp
+++ src/pages/login.lisp
@@ -9,12 +9,12 @@
        (with-params (:post name password)
          (if-let (user (validate-user name password))
            (progn (start-session user)
-                  "Login success!")
+                  (hunchentoot:redirect (hunchentoot:referer)))
            "Invalid credentials")))
       (:get (hunchentoot:handle-static-file page)))))
 
 (defroute logout "/logout"
   (if (hunchentoot:session hunchentoot:*request*)
       (progn (end-session)
-             "Logged out.")
+             (hunchentoot:redirect (hunchentoot:referer)))
       "You're not logged in!"))
