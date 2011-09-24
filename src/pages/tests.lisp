c30b458dabdbc9ab1d8d15ebf73b319c4b63e917
Benjamin Saunders <besaunde@sfu.ca>
2011-09-24 05:53:34 -0700

    Bind *string-modifier* at wider scope in the hopes of fixing horrid encoding breakage.

--- src/pages/tests.lisp
+++ src/pages/tests.lisp
@@ -1,17 +1,17 @@
 (in-package :quest)
 
 (defroute bbtest "/bbtest"
-  (ecase (request-method*)
-    (:post
-     (with-params (:post bbcode)
-       (let ((template:*string-modifier* #'identity))
+  (let ((template:*string-modifier* #'identity))
+   (ecase (request-method*)
+     (:post
+      (with-params (:post bbcode)
         (template:fill-and-print-template (find-template "bbtest")
                                           (let ((tree (with-input-from-string (in bbcode)
                                                         (parse-bbcode in))))
-                                           (list :parsed (escape-for-html (write-to-string tree))
-                                                 :rendered (with-output-to-string (out)
-                                                             (bbcode->html out tree))))
-                                          :stream (client-stream "application/xhtml+xml")))))
-    (:get (template:fill-and-print-template (find-template "bbtest")
-                                            (list :parsed nil)
-                                            :stream (client-stream "application/xhtml+xml")))))
+                                            (list :parsed (escape-for-html (write-to-string tree))
+                                                  :rendered (with-output-to-string (out)
+                                                              (bbcode->html out tree))))
+                                          :stream (client-stream "application/xhtml+xml"))))
+     (:get (template:fill-and-print-template (find-template "bbtest")
+                                             (list :parsed nil)
+                                             :stream (client-stream "application/xhtml+xml"))))))
