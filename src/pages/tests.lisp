dd547a9d543c85e0a87346665a5b79c431bafdc9
Benjamin Saunders <besaunde@sfu.ca>
2011-09-24 18:48:46 -0700

    More usable bbcode test page.

--- src/pages/tests.lisp
+++ src/pages/tests.lisp
@@ -4,14 +4,16 @@
   (ecase (request-method*)
     (:post
      (with-params (:post bbcode)
-       (format t "Input: ~A" bbcode)
        (template:fill-and-print-template #p"bbtest.tmpl"
                                          (let ((tree (with-input-from-string (in bbcode)
                                                        (parse-bbcode in))))
-                                           (list :parsed (escape-for-html (write-to-string tree))
+                                           (list :input (escape-for-html bbcode)
+                                                 :parsed (escape-for-html (write-to-string tree))
                                                  :rendered (with-output-to-string (out)
                                                              (bbcode->html out tree))))
                                          :stream (client-stream "text/html"))))
     (:get (template:fill-and-print-template #p"bbtest.tmpl"
-                                            (list :parsed nil)
+                                            (list :input ""
+                                                  :parsed nil
+                                                  :rendered nil)
                                             :stream (client-stream "text/html")))))
