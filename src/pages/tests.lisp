f6c061b883ce2b0b62f1e15351cb4742ed42b8e3
Benjamin Saunders <besaunde@sfu.ca>
2011-09-26 00:59:56 -0700

    Catch and display bbcode errors.

--- src/pages/tests.lisp
+++ src/pages/tests.lisp
@@ -5,11 +5,17 @@
    #p"bbtest.tmpl"
    (ecase (request-method*)
      (:post (with-params (:post bbcode)
-              (let ((tree (with-input-from-string (in bbcode)
-                            (parse-bbcode in))))
-                (list :input (escape-for-html bbcode)
-                      :parsed (escape-for-html (write-to-string tree))
-                      :rendered (with-output-to-string (out)
-                                  (bbcode->html out tree))))))
-     (:get (list :input "" :parsed nil :rendered nil)))
+              (handler-case
+                  (let ((tree (with-input-from-string (in bbcode)
+                                (parse-bbcode in))))
+                    (list :error nil
+                          :input (escape-for-html bbcode)
+                          :parsed (escape-for-html (write-to-string tree))
+                          :rendered (with-output-to-string (out)
+                                      (bbcode->html out tree))))
+                (bbcode-error (e) (list :error (princ-to-string e)
+                                        :input (escape-for-html bbcode)
+                                        :parsed nil
+                                        :rendered nil)))))
+     (:get (list :error nil :input "" :parsed nil :rendered nil)))
    :stream (client-stream "text/html")))
